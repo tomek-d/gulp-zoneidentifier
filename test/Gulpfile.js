83c8dc65f3bab6e43dc489377e6780712bebd055
Tomek Dyla <tomek-d@outlook.com>
2016-06-04 23:34:06 +0200

    Very first and fake unit test

--- test/Gulpfile.js
+++
@@ -1,9 +0,0 @@
-
-var gulp = require('gulp');
-var zoneIdentifier = require('../.')
-
-gulp.task('default', function () {
-  return gulp.src('in.js')
-    .pipe(zoneIdentifier({zoneId: 2}))
-    .pipe(gulp.dest('output'));
-})
---
+++ test/main.js
@@ -0,0 +1,11 @@
+var assert = require('stream-assert');
+var should = require('should');
+var zoneIdentifier = require('../');
+
+require('mocha');
+
+describe('gulp-zoneidentifier', function () {
+  it('should equal 5', function () {
+    (5).should.be.exactly(5);
+  });
+})
